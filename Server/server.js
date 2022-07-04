c068e47f4c0b38cb7e269c0871c24e1f63967f9c
sigltech <101203107+sigltech@users.noreply.github.com>
2022-07-04 01:19:10 +0100

    getting server running on heroku

--- Server/server.js
+++ Server/server.js
@@ -5,6 +5,9 @@ const server = express();
 server.use(express.json());
 server.use(cors());
 
+const controllers = require("./controllers");
+server.use("/tasks", controllers.tasks)
+
 server.get('/', (req,res) => res.send('Welcome to the server!'))
 
 module.exports = server;
